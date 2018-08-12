2421deb3b5c194516d00e01416c05f995317cc43
Maciej Kasprzyk <kontakt@kasprzykmaciej.pl>
2018-08-12 23:18:45 +0200

    Added button and empty GameActivity.

--- app/src/main/java/pl/kasprzykmaciej/szpieg/MainActivity.java
+++ app/src/main/java/pl/kasprzykmaciej/szpieg/MainActivity.java
@@ -1,13 +1,55 @@
 package pl.kasprzykmaciej.szpieg;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mPlayButton;
+    Button mSettingsButton;
+    Button mHelpButton;
+    Button mPlacesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        initViewByIDs();
+
+        setButtonsOnClickListeners();
+
+
+    }
+
+    private void setButtonsOnClickListeners() {
+
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(MainActivity.this , SettingsActivity.class);
+                //startActivity(intent);
+            }
+        });
+    }
+
+    private void initViewByIDs() {
+        mPlayButton = findViewById(R.id.play_intent_button);
+        mSettingsButton = findViewById(R.id.settings_intent_button);
+        mHelpButton = findViewById(R.id.help_intent_button);
+        mPlacesButton = findViewById(R.id.places_intent_button);
     }
 }
